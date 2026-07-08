a12b1d56675acda81e87debfbd05242ffe4a0e83
Mehrshad <megh92701@gmail.com>
2026-07-09 00:38:34 +0330

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -8,34 +8,60 @@ export interface Card {
 }
 function App() {
   const cardImages1 = [
-    { src: "blob/main/src/assets/photos/Rabbit.png", matched: false },
-    { src: "blob/main/src/assets/photos/RabbitStars.png", matched: false },
-    { src: "blob/main/src/assets/photos/CatZeroPercent.png", matched: false },
-    { src: "blob/main/src/assets/photos/Mouse.jpg", matched: false },
-    { src: "blob/main/src/assets/photos/OmnomSpider.png", matched: false },
-    { src: "blob/main/src/assets/photos/SleepyCat.png", matched: false },
+    { src: "Memory-game/assets/photos/Rabbit.png", matched: false },
+    { src: "Memory-game/assets/photos/RabbitStars.png", matched: false },
+    { src: "Memory-game/assets/photos/CatZeroPercent.png", matched: false },
+    { src: "Memory-game/assets/photos/Mouse.jpg", matched: false },
+    { src: "Memory-game/assets/photos/OmnomSpider.png", matched: false },
+    { src: "Memory-game/assets/photos/SleepyCat.png", matched: false },
   ];
   const cardImages2 = [
-    { src: "blob/main/src/assets/3/Untitled-1.jpg", matched: false },
-    { src: "blob/main/src/assets/3/Untitled-2.jpg", matched: false },
-    { src: "blob/main/src/assets/3/Untitled-3.jpg", matched: false },
-    { src: "blob/main/src/assets/3/Untitled-4.jpg", matched: false },
-    { src: "blob/main/src/assets/3/Untitled-5.jpg", matched: false },
-    { src: "blob/main/src/assets/3/Untitled-6.jpg", matched: false },
+    { src: "Memory-game/assets/3/Untitled-1.jpg", matched: false },
+    { src: "Memory-game/assets/3/Untitled-2.jpg", matched: false },
+    { src: "Memory-game/assets/3/Untitled-3.jpg", matched: false },
+    { src: "Memory-game/assets/3/Untitled-4.jpg", matched: false },
+    { src: "Memory-game/assets/3/Untitled-5.jpg", matched: false },
+    { src: "Memory-game/assets/3/Untitled-6.jpg", matched: false },
   ];
   const cardImages3  = [
-    { src: "blob/main/src/assets/1/Untitled-1.jpg", matched: false },
-    { src: "blob/main/src/assets/1/Untitled-2.jpg", matched: false },
-    { src: "blob/main/src/assets/1/Untitled-3.jpg", matched: false },
-    { src: "blob/main/src/assets/1/Untitled-4.jpg", matched: false },
-    { src: "blob/main/src/assets/1/Untitled-5.jpg", matched: false },
-    { src: "blob/main/src/assets/1/Untitled-6.jpg", matched: false },
+    { src: "Memory-game/assets/1/Untitled-1.jpg", matched: false },
+    { src: "Memory-game/assets/1/Untitled-2.jpg", matched: false },
+    { src: "Memory-game/assets/1/Untitled-3.jpg", matched: false },
+    { src: "Memory-game/assets/1/Untitled-4.jpg", matched: false },
+    { src: "Memory-game/assets/1/Untitled-5.jpg", matched: false },
+    { src: "Memory-game/assets/1/Untitled-6.jpg", matched: false },
   ];
   const [cards, setCards] = useState<Card[]>([]);
   const [turns, setTurns] = useState(0);
-  const [firstChoice, setFirstChoice] = useState<Card | null>(null);
-  const [secondChoice, setSecondChoice] = useState<Card | null>(null);
+  const [firstChoice, setFirstChoice] = useState<Card | null>();
+  const [secondChoice, setSecondChoice] = useState<Card | null>();
   const [disabled, setDisabled] = useState(false);
+  const [finished, setFinished] = useState(false);
+  const [showWinGif, setShowWinGif] = useState(false);
+  const [finishedCard, setFinishedCard] = useState(false);
+
+  useEffect(() => {
+    if (cards.length > 0 && cards.every(card => card.matched === true)) {
+      setFinished(true);
+      setShowWinGif(true);
+    }
+  }, [cards]);
+
+  useEffect(() => {
+    if (finished) {
+      setFinishedCard(true);
+      // const playAgain = window.confirm("🎉 شما برنده شدید! آیا می‌خواهید دوباره بازی کنید؟");
+      setShowWinGif(true);
+      setTimeout(() => {
+        setShowWinGif(false);
+        console.log("gif end");
+      }, 1900);
+      // if (playAgain) {
+      //   ShuffleCard();
+      // }
+      setFinished(false);
+    }
+  }, [finished]);
 
   const ShuffleCard = () => {
     const packs = [cardImages1,cardImages2,cardImages3];
@@ -49,11 +75,14 @@ function App() {
     setSecondChoice(null)
     setCards(shuffledCards);
     setTurns(0);
+    setFinished(false);
+    setShowWinGif(false);
   };
+
   const handleCoice = (card: Card) => {
     firstChoice ? setSecondChoice(card) : setFirstChoice(card);
-    console.log(firstChoice)
-    console.log(secondChoice)
+    // console.log(firstChoice?.matched)
+    // console.log(secondChoice?.matched)
   };
 
   //compare 2 Selected Cards
@@ -91,8 +120,21 @@ function App() {
   },[])
   return (
     <div className="App">
+      <img 
+        className={ showWinGif ? "finished" : "image"}
+        src="Memory-game/assets/finished.gif"
+        alt="Celebration!"
+        />
       <h1>Memory Game</h1>
       <button onClick={ShuffleCard}>New Game</button>
+      <div className={ finishedCard ? "FinishedCard" : "image"}>
+        You won. wanna play again?
+        <span>
+          <button onClick={()=>{ShuffleCard();setFinishedCard(false)}}>New Game</button>
+          <button onClick={()=>setFinishedCard(false)}>No Thanks</button>
+        </span>
+        
+      </div>
       <div className="card-grid">
         {cards &&
           cards.map((card) => (
@@ -103,8 +145,10 @@ function App() {
               flipped={card===firstChoice || card===secondChoice || card.matched}
               disabled = {disabled}
               />
-          ))}
+          ))
+        }
       </div>
+      
       <p>Turns : {turns}</p>
     </div>
   );
