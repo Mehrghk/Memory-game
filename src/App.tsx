b67e0f52f3d8bfc2250bcad54521ea9e0ff7f854
Mehrshad <megh92701@gmail.com>
2026-07-18 18:50:48 +0330

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,12 @@ export interface Card {
 }
 function App() {
   const cardImages1 = [
-    { src: "Memory-game/assets/photos/Rabbit.png", matched: false },
-    { src: "Memory-game/assets/photos/RabbitStars.png", matched: false },
-    { src: "Memory-game/assets/photos/CatZeroPercent.png", matched: false },
-    { src: "Memory-game/assets/photos/Mouse.jpg", matched: false },
-    { src: "Memory-game/assets/photos/OmnomSpider.png", matched: false },
-    { src: "Memory-game/assets/photos/SleepyCat.png", matched: false },
+    { src: "/Memory-game/assets/photos/Rabbit.png", matched: false },
+    { src: "/Memory-game/assets/photos/RabbitStars.png", matched: false },
+    { src: "/Memory-game/assets/photos/CatZeroPercent.png", matched: false },
+    { src: "/Memory-game/assets/photos/Mouse.jpg", matched: false },
+    { src: "/Memory-game/assets/photos/OmnomSpider.png", matched: false },
+    { src: "/Memory-game/assets/photos/SleepyCat.png", matched: false },
   ];
   const cardImages2 = [
     { src: "Memory-game/assets/3/Untitled-1.jpg", matched: false },
